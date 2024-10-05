553ef446f9bf612663dd828b197c04779847eb6b
jzscsaba <johanyak.csaba@nje.hu>
2024-10-05 09:52:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.OleDb;
+namespace CnAN_Providers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Installed OLEDb data providers:");	
+			OleDbDataReader rdr = OleDbEnumerator.GetRootEnumerator();
+			while (rdr.Read())
+			{
+				Console.WriteLine(String.Format("Name = {0}, Description = {1}", rdr[0], rdr[2]));
+			};
+		}
+	}
+}
